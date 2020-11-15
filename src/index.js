c86e87a62cce477423afb292fa9014a32e30ad5b
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-15 18:09:43 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 8080;
 app.use(express.urlencoded());
-
+let studentsArr = require("./InitialData.js");
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // your code goes here
 
+app.get("/api/student", (req, res) => {
+  res.json(studentsArr);
+});
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
-
-module.exports = app;   
+module.exports = app;
